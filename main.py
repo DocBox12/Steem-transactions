c0b30be7bd873d0162eb3c2cc3cab111de9e973b
GratisLPG <socialmedia@pawelpieczyrak.pl>
2019-03-05 15:45:29 +0100

    Add get_fill_order function

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def get_claim_rewards():
     all_transactions = Account('docbox', steem).get_account_history(-1, 100, filter_by='claim_reward_balance', raw_output=True)
 
     for data in all_transactions:
+        print(data)
         DICT_details = data[1]
 
      
@@ -41,3 +42,34 @@ def get_claim_rewards():
         
     return
 
+def get_fill_order():
+
+    all_transactions = Account('docbox', steem).get_account_history(-1, 100, filter_by='fill_order', raw_output=True)
+
+    for data in all_transactions:
+        DICT_details = data[1]
+
+        trx_id = DICT_details.get("trx_id")
+        block = DICT_details.get("block")
+        trx_in_block = DICT_details.get("trx_in_block")
+        op_in_trx = DICT_details.get("op_in_trx")
+        virtual_op = DICT_details.get("virtual_op")
+        timestamp = DICT_details.get("timestamp")
+
+        human_time = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+
+        LIST_op = DICT_details.get("op")
+
+        DICT_op = LIST_op[1]
+        current_owner = DICT_op.get("current_owner")
+        current_orderid = DICT_op.get("current_orderid")
+        current_pays = DICT_op.get("current_pays")
+        current_pays = DICT_op.get("current_pays")
+        open_orderid = DICT_op.get("open_orderid")
+        open_pays = DICT_op.get("open_pays")
+
+        # Generate link to block
+
+        link = "https://steemworld.org/block/" + str(block) + "/" + str(trx_id)
+
+    return
