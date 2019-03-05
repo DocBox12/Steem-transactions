f8b917a8870aec183d7e34c1cf334ed011ec3b94
GratisLPG <socialmedia@pawelpieczyrak.pl>
2019-03-05 15:57:15 +0100

    Add get_transfers function

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ def get_claim_rewards():
     all_transactions = Account('docbox', steem).get_account_history(-1, 100, filter_by='claim_reward_balance', raw_output=True)
 
     for data in all_transactions:
-        print(data)
         DICT_details = data[1]
-
-     
 
         trx_id = DICT_details.get("trx_id")
         block = DICT_details.get("block")
@@ -71,5 +68,39 @@ def get_fill_order():
         # Generate link to block
 
         link = "https://steemworld.org/block/" + str(block) + "/" + str(trx_id)
+
+    return
+
+
+def get_transfers():
+    all_transactions = Account('docbox', steem).get_account_history(-1, 100, filter_by='transfer', raw_output=True)
+
+    for data in all_transactions:
+        DICT_details = data[1]
+
+        trx_id = DICT_details.get("trx_id")
+        block = DICT_details.get("block")
+        trx_in_block = DICT_details.get("trx_in_block")
+        op_in_trx = DICT_details.get("op_in_trx")
+        virtual_op = DICT_details.get("virtual_op")
+        timestamp = DICT_details.get("timestamp")
+
+        human_time = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+
+        LIST_op = DICT_details.get("op")
+
+        DICT_op = LIST_op[1]
+        
+        from_ = DICT_op.get("from_")
+        to = DICT_op.get("to")
+        amount = DICT_op.get("amount")
+        memo = DICT_op.get("memo")
+
+        # Generate link to block
+
+        link = "https://steemworld.org/block/" + str(block) + "/" + str(trx_id)
+
+
+
 
     return
