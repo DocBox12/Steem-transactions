2d3f8fbfb93e756b5c020df7357c0ce41e6d510f
GratisLPG <socialmedia@pawelpieczyrak.pl>
2019-03-07 11:12:55 +0100

    Save all transactions to the database and fix create database

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def create_sql():
     current_owner TEXT NULL,
     current_orderid TEXT NULL,
     current_pays TEXT NULL,
+    open_owner TEXT NULL,
     open_orderid TEXT NULL,
     open_pays TEXT NULL,
     link TEXT NULL,
@@ -70,11 +71,14 @@ def create_sql():
     from_ TEXT NULL,
     to_ TEXT NULL,
     amount TEXT NULL,
-    memo TEXT NULL
+    memo TEXT NULL,
+    link TEXT NULL,
+    filled TEXT NULL
     );
     
     """)
 
+    cursor = mariadb_connection.cursor()
 
     cursor.execute(create_claim_rewards)
     mariadb_connection.commit()
@@ -95,6 +99,7 @@ def create_sql():
 
 
 def get_claim_rewards():
+    cursor = mariadb_connection.cursor()
 
     all_transactions = Account(steem_username, steem).get_account_history(-1, 100, filter_by='claim_reward_balance', raw_output=True)
 
@@ -123,10 +128,26 @@ def get_claim_rewards():
 
         link = "https://steemworld.org/block/" + str(block) + "/" + str(trx_id)
 
+        filled = ""
+
+        sql_insert = ("""
+        
+        INSERT INTO claim_rewards (trx_id, block, trx_in_block, op_in_trx, virtual_op, timestamp, account,reward_steem, reward_sbd, reward_vests, link, filled)
+        VALUES("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s", "%s");
+
+        """) % (str(trx_id), str(block), str(trx_in_block), str(op_in_trx), str(virtual_op), str(human_time), str(account), str(reward_steem), str(reward_sbd), str(reward_vests), str(link), str(filled))
+
+
+        cursor.execute(sql_insert)
+        mariadb_connection.commit()
+
+    mariadb_connection.close()
+
         
     return
 
 def get_fill_order():
+    cursor = mariadb_connection.cursor()
 
     all_transactions = Account(steem_username, steem).get_account_history(-1, 100, filter_by='fill_order', raw_output=True)
 
@@ -156,10 +177,26 @@ def get_fill_order():
 
         link = "https://steemworld.org/block/" + str(block) + "/" + str(trx_id)
 
+        filled = ""
+
+        sql_insert = ("""
+        
+        INSERT INTO fill_order (trx_id,	block, trx_in_block, op_in_trx, virtual_op,	timestamp,	current_owner, current_orderid,	current_pays, open_owner, open_orderid, open_pays, link, filled)
+        VALUES("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s", "%s", "%s", "%s");
+
+        """) % (str(trx_id), str(block), str(trx_in_block), str(op_in_trx), str(virtual_op), str(human_time), str(current_owner), str(current_orderid), str(current_pays), str(open_owner), str(open_orderid), str(open_pays), str(link), str(filled))
+
+
+        cursor.execute(sql_insert)
+        mariadb_connection.commit()
+    mariadb_connection.close()
+
     return
 
 
 def get_transfers():
+    cursor = mariadb_connection.cursor()
+
     all_transactions = Account(steem_username, steem).get_account_history(-1, 100, filter_by='transfer', raw_output=True)
 
     for data in all_transactions:
@@ -187,7 +224,19 @@ def get_transfers():
 
         link = "https://steemworld.org/block/" + str(block) + "/" + str(trx_id)
 
+        filled = ""
 
+        sql_insert = ("""
+        
+        INSERT INTO transfers (trx_id,	block, trx_in_block, op_in_trx, virtual_op,	timestamp, from_, to_, amount, memo, link, filled)
+        VALUES("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s", "%s");
+
+        """) % (str(trx_id), str(block), str(trx_in_block), str(op_in_trx), str(virtual_op), str(human_time), str(from_), str(to), str(amount), str(memo), str(link), str(filled))
+
+
+        cursor.execute(sql_insert)
+        mariadb_connection.commit()
+    mariadb_connection.close()
 
 
     return
@@ -209,4 +258,3 @@ steem_username = config['steem']['username']
 
 # SQL Connection
 mariadb_connection = mariadb.connect(user=user_db, password=password_db, database=database_db, host=address_db, port=INT_port_db)
-cursor = mariadb_connection.cursor()
